84a4e09731b572b597d90309909742108d3ecb85
Vipul Tiwari <er.vktcs@gmail.com>
2025-11-24 20:44:50 +0530

    Add basic CPP code Path for daily Practice

---
+++ Cpp/Basic/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int fav_number = 42;
+    cout<< "Enter you faviourite number" << endl;
+    cin>> fav_number;
+    cout<< "Your faviourite number is " << fav_number << endl;
+    return 0;
+}
